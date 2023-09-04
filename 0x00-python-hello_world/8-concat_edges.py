ed85c124626c34124ff57fe8579f8a08041d050d
kelechi nnadi <kelechinnadi019@gmail.com>
2023-09-04 16:16:31 +0100

    the update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = f'{str[39:-63]} {str[107:-18]} {str[0:-122]}'
+#tr = f'{str[39:-63]} {str[107:-18]} {str[0:-122]}'
+str = '{} {} {}'.format(str[39:-63], str[107:-18], str[0:-122])
 print(str)
