a9a2195b23f124523181bd903e4ab7a6bc018b8d
kelechi nnadi <kelechinnadi019@gmail.com>
2023-09-09 19:15:12 +0100

    the code

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 3-print_reversed_list_integer.py
+# kelechi nnadi <alx swe africa>
+
+def print_reversed_list_integer(my_list=[]):
+    """function that print the list in reverse
+    order"""
+    for i in my_list:
+        
