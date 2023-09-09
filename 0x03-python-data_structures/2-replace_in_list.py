c2ba46c683adb9fb0f581e9340ad6bf07cab2462
kelechi nnadi <kelechinnadi019@gmail.com>
2023-09-09 18:25:52 +0100

    the code

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,5 +7,6 @@ def replace_in_list(my_list, idx, element):
     at a specific position"""
     if idx < 0 or idx >= len(my_list):
         return my_list
-    my_list[idx] = element
-    return my_list
+    for i in my_list:
+        my_list[idx] = element
+        return my_list
