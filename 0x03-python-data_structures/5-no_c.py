eb1d5f6fbd6e14ad40eb36bd39f0c3a304e1c23f
kelechi nnadi <kelechinnadi019@gmail.com>
2023-09-12 23:39:33 +0100

    the code of return string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#5-no_c.py
+#kelechi nnadi <alx swe school>
+
+def no_c(my_string):
+    """function that removes all characters c
+    and C from a string"""
+    for i in my_string:
+        if  i == 'c' or i == 'C':
+            continue
+        print(i, end='')
