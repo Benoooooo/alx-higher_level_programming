ea8ec9c3adfb006ef90414acce3a118e5375db5d
kelechi nnadi <kelechinnadi019@gmail.com>
2023-09-15 09:47:05 +0100

    the code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,8 +5,5 @@
 def no_c(my_string):
     """function that removes all characters c
     and C from a string"""
-    for i in my_string:
-        if  i == 'c' or i == 'C':
-            continue
-        my_string[i]
-        return my_string
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return("".join(copy))
