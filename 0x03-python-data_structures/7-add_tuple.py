c1e9221deba2921f02f3a49acc7558f7b3c18e81
kelechi nnadi <kelechinnadi019@gmail.com>
2023-09-17 17:03:28 +0100

    the multiple_returns

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,12 +7,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     res1 = tuple_a[0] + tuple_b[0]
     res2 = tuple_a[1] + tuple_b[1]
-    tup = ()
-    for i in tup:
-        if i < 2:
-            tup = i + 0
-            return tup
-        else:
-            return tup
+    for a, b in tuple_a, tuple_b:
+        if a < 2 or b < 2:
+            return a
     return res1, res2
 
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 8-multiple_returns.py
+# kelechi nnadi <alx school africa>
+
+def multiple_returns(sentence):
+    """function that returns a tuple with tha length of a string
+    and its first characteer"""
+
+    if sentence == '':
+        sentence[0] = None
+        return sentence
+    else:
+        t = sentence[0]
+        r = len(sentence)
+        res = (r, t)
+        return res
