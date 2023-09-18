ed8054b67d964e26bd0236b7da9013ebdc3e56a4
kelechi nnadi <kelechinnadi019@gmail.com>
2023-09-18 07:55:24 +0100

    the code

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 9-max_integer.py
+# kelechi nnadi <alx swe africa>
+
+def max_integer(my_list=[]):
+    """ function that prints the biggest integer
+    and not allowed to use max() function if the
+    list is empty return  None"""
+    if my_list == []:
+        return None
+    largest = my_list[0]
+    for i in range(1, len(my_list)):
+       if my_list[1] > largest:
+           largest = my_list[i]
+    return largest
+       
