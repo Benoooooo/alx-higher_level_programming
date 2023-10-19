a7b9cdee7df9ff4771127973a3f172b3af726674
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-19 18:26:12 +0100

    the code

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# file: 4-print_hexa.py
+# Auth: kelechi nnadi <alx swe>
+
+for i in range(0, 99):
+    print("{:d} = {}".format(i, hex(i)))
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Auth: kelechi nnadi <alx swe>
+# file: 5-print_comb2.py
+
+for i in range(0, 100):
+    print("{:d}, ".format(i), end="")
+print()
