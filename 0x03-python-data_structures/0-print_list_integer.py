a68bb86b2eee099c64398e29db8a0e894a469de0
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-26 05:56:37 +0100

    deletes

--- 0x03-python-data_structures/0-print_list_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# 0-print_list_integer
-# kelechi nnadi <alx SE schoool africa>
-
-def print_list_integer(my_list=[]):
-    """function that prints all integer of a list"""
-    for i in my_list:
-        print("{:d}".format(i))
--- 0x03-python-data_structures/1-element_at.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 1-element_at.py
-# kelechi nnadi <alx-swe africa>
-
-def element_at(my_list, idx):
-    """function that retrieves an element from a list"""
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
