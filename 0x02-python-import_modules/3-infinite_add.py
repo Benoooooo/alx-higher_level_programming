a2a5aeb6435f58138216fddbbedb68cb8c4897f1
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-06 16:39:21 +0100

    the code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-# 3-infinite_add.py
-# Brennan D Baraban <375@holbertonschool.com>
+# file: 3-infinite_add.py
+# Auth: kelechi nnadi <@alx swe>
 
-if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
+if __name__ == '__name__':
+    """print the result of the
+    addition of all arguments"""
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    
