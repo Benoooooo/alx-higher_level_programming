32bfb0c568baaf9f963768908bc3b1d7bdd28b14
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-07 10:14:17 +0100

    the code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,9 @@
 # file: 3-infinite_add.py
 # Auth: kelechi nnadi <@alx swe>
 
-if __name__ == '__name__':
-    """print the result of the
-    addition of all arguments"""
+import sys
+import math
 
-    
+n = int(sys.argv[1])
+for i in range(n):
+    print(i + n)
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# file: 5-variable_load.py
+# Auth: kelechi nnadi <@alx swe>
+
+if __name__ == '__main__':
+    """ 
+    the program should import the variable a
+    form the file variable_load_5.py and prints it
+    value
+    """
+
+    from variable_load_5 import a
+
+    print(a)
