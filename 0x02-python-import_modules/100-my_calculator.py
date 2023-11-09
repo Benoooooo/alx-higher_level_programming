77bb0d88ccb36ff37f87102a2f94b54d9bed4368
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-09 09:19:22 +0100

    the code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,10 +6,32 @@ if __name__ == '__main__':
     """ program that imports all functions from the file
     calculator_1.py and handles basic opeations """
     import calculator_1
+    import sys
 
     n = len(sys.argv) - 1
 
-    if n < 3:
+    if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit 1
-    elif 
+        print(1)
+    else:
+       addi = lambda a, b: a + b
+       subi = lambda a, b: a - b
+       divi = lambda a, b: a / b
+       muli = lambda a, b: a * b
+       res = 0
+       for i in sys.argv:
+           res += int(i)
+       if op != addi:
+           print("Unknown operator. Available operators: +, -, * and /")
+           print(1)
+       elif op != subi:
+           print("Unknown operator. Available operators: +, -, * and /")
+           print(1)
+       elif op != divi:
+           print("Unknown operator. Available operators: +, -, * and /")
+           print(1)
+       elif op != muli:
+           print("Unknown operator. Available operators: +, -, * and /")
+           print(1)
+       print("{} {} {} = {} ".format(a, op, b, calculator_1(a, b)))
+
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,18 +2,7 @@
 # file: 4-hidden_discovery.py
 # Auth: kelechi nnadi <@alx swe>
 
-if __name__ == '__main__':
-    """ function that import module"""
-    import importlib.util
+import dis
 
-    name = 'path/to/hidden_4.pyc'
-    spec = importlib.util.spec_from_file_location('hidden_4', name)
-
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    name_defined = dir(module)
-
-    for name in sorted(names_defined):
-        if not name.startswith("__"):
-            print(name)
+for i in hidden_4:
+    print(i)
