702ff76f6cdd8d68c7692fb5614e40569003349f
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-09 05:34:54 +0100

    the code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,11 @@
 import sys
 import math
 
-n = int(sys.argv[1])
-for i in range(n):
-    print(i + n)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        result = 0
+        for arg in sys.argv[1:]:
+            result += int(arg)
+        print(result)
+    else:
+        print(0)
