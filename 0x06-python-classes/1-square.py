67fbe1c4b641f923b4afbb6e91df91c84c2fc69e
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-10 21:07:40 +0100

    the 1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# file: 1-square.py
+# Auth: kelechi nnadi <@alx swe>
+
+""" defining a square class"""
+
+
+class Square:
+    """wiriting a square class"""
+
+    def __init__(self, size):
+        self._size = size
