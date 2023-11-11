aa8b0df7217e1b36d4c343266ade927b7eb00fd4
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-11 03:08:53 +0100

    the code

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,14 +9,12 @@ class Square:
     """ class defining the data of square"""
 
     def __init__(self, size):
-        """ function"""
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        """initializing the size of file if checked"""
 
     def area(self):
         return self.__size ** 2
