abacb844601601bc66db8c38c21823883bcb4413
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-11 03:00:55 +0100

    the code

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# file: 3-square.py
+# Auth: kelechi nnadi <@alx swe>
+
+"""class definition"""
+
+
+class Square:
+    """ class defining the data of square"""
+
+    def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ check the area of the size and return
+        """
+
+        return self.__size ** 2
