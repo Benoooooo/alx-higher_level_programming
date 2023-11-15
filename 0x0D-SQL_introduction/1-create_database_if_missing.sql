e065cfde12d11af437f13b2f0f58464a95af39b5
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-15 03:09:43 +0100

    the code

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- file: 1-create_database_if_missing.sql
+-- Auth: kelechi nnadi <@alx swe>
+-- a script that create a database hbtn_0c_0 in your MySQL server
+
+CREATE DATABASE[IF NOT EXIT]hbtn_0c_0
