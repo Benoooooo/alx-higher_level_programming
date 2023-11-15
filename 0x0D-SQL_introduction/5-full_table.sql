a5fbf335525f24fd9a54ef0ce9757b6d09e35b79
kelechi nnadi <kelechinnadi019@gmail.com>
2023-11-15 04:20:12 +0100

    the code

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- file: 5-full_table.sql
+-- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+
+SHOW COLUMNS FROM first_table FROM hbtn_0c_0;
