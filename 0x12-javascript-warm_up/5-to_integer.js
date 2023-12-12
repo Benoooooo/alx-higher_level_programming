6407a79d72f83e9ea486899a60fd1e9308a75680
kelechi nnadi <kelechinnadi019@gmail.com>
2023-12-12 03:50:44 +0100

    the code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
+const firstArg = args[0];
 
-if (!(args.length === 1 && !isNaN(parseInt(args[0])))) {
-  console.log('Not a number');
+if (Number.isInteger(parseInt(firstArg))) {
+	console.log(`My number: ${parseInt(firstArg)}`);
 } else {
-  console.log("My number: ", parseInt(args[0]));
+	console.log('Not a number');
 }
+
