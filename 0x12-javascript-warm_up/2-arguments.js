1ac3d9924414e944127445debbf960cba019b734
kelechi nnadi <kelechinnadi019@gmail.com>
2023-12-12 01:28:42 +0100

    the code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-// const { argv } = require('node:process');
-
 if (process.argv.length <= 2) {
   console.log('No argument');
 } else {
-  console.log('Argument found');
+  console.log('Arguments found');
 }
