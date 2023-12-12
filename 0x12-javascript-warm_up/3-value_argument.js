78c5f0159254b431b34bb2d7059712207daed4e8
kelechi nnadi <kelechinnadi019@gmail.com>
2023-12-12 02:22:42 +0100

    the code

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-if (args === 0) {
-  console.log("No argument");
-} else if (args.length === 1) {
-  console.log("Argument found");
+if (!args[0]) {
+  console.log('No argument');
 } else {
-  console.log("Arguments found");
+  console.log(args[0]);
 }
