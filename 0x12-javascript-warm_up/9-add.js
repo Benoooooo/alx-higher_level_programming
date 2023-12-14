8d379b73684b28f3d7f418453a47976e1f4b841b
kelechi nnadi <kelechinnadi019@gmail.com>
2023-12-14 04:12:53 +0100

    the code

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,11 +3,11 @@ const args = process.argv.slice(2);
 const first = parseInt(args[0]);
 const sec = parseInt(args[1]);
 
-function add(a, b) {
+function add (a, b) {
   if (Number.isInteger(a) && Number.isInteger(b)) {
     return a + b;
   } else {
-    return "NAN";
+    return 'NAN';
   }
 }
 console.log(add(first, sec));
