df5a1ead18f71c3ba1a8ca05bac7175af6e37f22
kelechi nnadi <kelechinnadi019@gmail.com>
2024-01-08 21:42:43 +0100

    the code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,23 +6,14 @@ displays the body of the response (decode in utf-8)
 
 import urllib.request
 import sys
-from urllib.error import URLError, HTTPError
+import urllib.error
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        decoded_body = body.decode('utf-8')
-
     try:
-        res = urllib.request.urlopen(decoded_body)
-    except HTTPError as e:
-        print('Thes server couldn\'t fulfill the request.')
-        print('Error code: ', e.code)
-    except URLError as e:
-        print('We failed to reach a server.')
-        print('Reason: ', e.reason)
-    else:
-        print("result successful")
-
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
