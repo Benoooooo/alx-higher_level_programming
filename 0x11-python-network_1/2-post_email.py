b6f83b4290016c0414f50f27eaf11c5092d7e30f
kelechi nnadi <kelechinnadi019@gmail.com>
2024-01-08 12:53:08 +0100

    the code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,10 +9,10 @@ import urllib.parse
 import urllib.request
 
 if __name__ == "__main__":
-	url = sys.argv[1]
-	email = sys.argv[2]
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-	data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-	with urllib.request.urlopen(url, data) as response:
-		body = response.read().decode('utf-8')
-		print(body)
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read().decode('utf-8')
+        print(body)
