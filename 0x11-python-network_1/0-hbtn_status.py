a4d9819cb8d9d6fb0f4514c310f3c92eef4bb179
kelechi nnadi <kelechinnadi019@gmail.com>
2024-01-08 05:20:33 +0100

    the ocde

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,7 @@
 if __name__ == "__main__":
     import urllib.request
 
-    url = 'https://alx-intranet.hbtn.io/status'
-
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
