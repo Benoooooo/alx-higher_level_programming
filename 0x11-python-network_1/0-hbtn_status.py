e36e9de8369c1ac66e27fd3ad971bb7a4d769d8c
kelechi nnadi <kelechinnadi019@gmail.com>
2024-01-08 05:37:49 +0100

    the code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-# write a script that fetches url
+'''
+write a script that fetches url
+'''
+
+import urllib.request as request
 
 if __name__ == "__main__":
-    import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(repr(body)))
+        print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode('utf-8')))
