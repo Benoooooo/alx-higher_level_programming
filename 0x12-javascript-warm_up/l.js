a5d89c3aea14834058feb30bc0730e3fd95c8ad2
kelechi nnadi <kelechinnadi019@gmail.com>
2024-01-10 01:03:20 +0100

    the code

--- 0x12-javascript-warm_up/l.js
+++ 0x12-javascript-warm_up/l.js
@@ -2,8 +2,8 @@
 let index = 0;
 let index1 = 0;
 
-let size = 2;
-let size2 = 2;
+const size = 2;
+const size2 = 2;
 for (index = 0; index <= size; index++) {
   for (index1 = 0; index1 <= size; index1++) {
   }
