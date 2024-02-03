55a5325a7d3e16d2ac5bf3f9f4250916834cbaa5
kelechi nnadi <kelechinnadi019@gmail.com>
2024-02-03 07:34:59 +0100

    the code

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,34 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
-
-// Function to count completed tasks by user ID
-const countCompletedTasksByUserId = (todos) => {
-  const completedTasksByUserId = {};
-
-  // Count completed tasks for each user
-  todos.forEach((todo) => {
-    if (todo.completed) {
-      const userId = todo.userId;
-      completedTasksByUserId[userId] = completedTasksByUserId[userId] + 1 || 1;
-    }
-  });
-
-  return completedTasksByUserId;
-};
-
-// Make a GET request to the API URL
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
     const todos = JSON.parse(body);
-    const completedTasksByUserId = countCompletedTasksByUserId(todos);
-
-    // Print users with completed tasks
-    Object.keys(completedTasksByUserId).forEach((userId) => {
-      console.log(`User ${userId} completed ${completedTasksByUserId[userId]} task(s).`);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
     });
+    console.log(completed);
   }
 });
