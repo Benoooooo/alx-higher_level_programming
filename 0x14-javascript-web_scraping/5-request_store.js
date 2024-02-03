1eb402efca56556e2540229f492352eaa68006e9
kelechi nnadi <kelechinnadi019@gmail.com>
2024-02-03 07:29:02 +0100

    the code

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,4 @@
 #!/usr/bin/node
+const fs = require('fs');
 const request = require('request');
-
-const apiUrl = process.argv[2];
-const characterId = 18;
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const moviesWithWedgeAntilles = films.filter(film => {
-      const characters = film.characters.map(url => url.split('/').slice(-2, -1)[0]);
-      return characters.includes(characterId.toString());
-    });
-    console.log(moviesWithWedgeAntilles.length);
-  }
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
